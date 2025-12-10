692898fbf17070e965e73dbe94798f37818604d5
Your Name <146244420+Ahmed-Abdelgwad@users.noreply.github.com>
2025-12-10 16:50:30 +0200

    feat: Add Django framework and its dependencies to the project.

--- lesson2/myproject/myproject/urls.py
+++ lesson2/myproject/myproject/urls.py
@@ -22,6 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home),
     path('about', views.about),
-    path('posts/', include('posts.urls'))
+    path('posts/', include('posts.urls')),
 ]
 
